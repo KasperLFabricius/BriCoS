e0b3dce6bb1a179b0274ba92325ebbbea96565af
KasperLFabricius <kasperlindskov.fabricius@sweco.dk>
2026-06-04 10:51:55 +0200

    Update bricos_data.py
    Updated version to 0.41

--- bricos_data.py
+++ bricos_data.py
@@ -11,7 +11,7 @@ import time
 # GLOBAL CONFIGURATION
 # ==========================================
 
-APP_VERSION = "0.40"
+APP_VERSION = "0.41"
 AUTOSAVE_FILE = "latest_session.csv"
 
 # ==========================================
